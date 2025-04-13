a50b47f0bb86425d7d3f3786376171c10c9ad786
ankit-yadav90 <ankityadav73699@gmail.com>
2025-04-13 20:29:45 +0530

    check vowels

---
+++ first.js
@@ -0,0 +1,23 @@
+//check Vowels
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+        }
+
+        return count;
+}
+
+//check vowels arrow function
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+        }
+
+        return count;
+}
